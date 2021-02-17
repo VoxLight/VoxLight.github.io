8d07bb9c885b209da14be22a9991235b3cca0edb
VoxLight <tkkt392@gmail.com>
2021-02-17 03:02:11 -0500

    Edited create_monster_row

--- SRPGCalc/script.js
+++ SRPGCalc/script.js
@@ -3,14 +3,13 @@ const MONSTER_TABLE_NAME = "monstertable";
 const MONSTER_TABLE_BODY = "monstertablebody";
 const STATS_FORM_NAME = "calc";
 
-const EX_TR = document.getElementById(ROW_NAME).content.cloneNode(true);
-const MONSTER = ["Wolf", 5, 0, 0, 0, 0, 0, 0, 0]
+var ex_tr = document.getElementById(ROW_NAME).content.cloneNode(true);
+var monster = ["Wolf", 5, 0, 0, 0, 0, 0, 0, 0]
 
 function create_monster_row(monster){
-    let row = EX_TR.cloneNode(true);
-    let td = row.querySelectorAll("td");
-    for(let i =0; i<MONSTER.length; i++){
-        td[i] = MONSTER[i];
+    let row = ex_tr.cloneNode(true);
+    for(let i =0; i<monster.length; i++){
+        row.children[0].cells[i].innerHTML = monster[i];
     }
     return row
 };
@@ -23,7 +22,7 @@ function insert_monster(row){
 
 function populate_monster_table(){
     setInterval(function(){
-        insert_monster(create_monster_row(MONSTER));
+        insert_monster(create_monster_row(monster));
     }, 1000)
 
     return false;
