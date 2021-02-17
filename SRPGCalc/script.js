8ed03205f374eb9643c4a2d169f14ce89c71aec8
VoxLight <tkkt392@gmail.com>
2021-02-16 21:45:58 -0500

    Rediesigned the form
    Redesigned the forma nd changed the layout. Added some JS functionality behind it.

--- SRPGCalc/script.js
+++ SRPGCalc/script.js
@@ -1,1 +1,17 @@
 console.log("Loaded script.js")
+
+
+
+
+document.getElementById('calculator-input').onsubmit = function() { 
+    console.log(document.getElementById('fights').value);
+    console.log(document.getElementById('stat2').value);
+    console.log(document.getElementById('stat3').value);
+    console.log(document.getElementById('stat4').value);
+    console.log(document.getElementById('stat5').value);
+
+    return false;
+
+
+
+};
