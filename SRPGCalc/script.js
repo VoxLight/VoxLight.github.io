85d4088378174e4651b9cf812a464e8077e15d50
VoxLight <tkkt392@gmail.com>
2021-02-16 22:07:27 -0500

    Added some style
    Added some more style, as well as some javascript to test somethings.

--- SRPGCalc/script.js
+++ SRPGCalc/script.js
@@ -2,6 +2,18 @@ console.log("Loaded script.js")
 
 
 
+function update_progress_bar(amount){
+    let bar = document.getElementById('progress-bar');
+    bar.setAttribute("aria-valuenow", amount);
+    return false;
+}
+
+setInterval(function(){
+    let bar = document.getElementById('progress-bar');
+    let amount = bar.getAttribute("aria-valuenow");
+    update_progress_bar(amount+3);
+}, 3000);
+
 
 document.getElementById('calculator-input').onsubmit = function() { 
     console.log(document.getElementById('fights').value);
