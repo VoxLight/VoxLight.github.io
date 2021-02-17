deb3c1453ed9a6b24b0194802995ff712ca09dd4
VoxLight <tkkt392@gmail.com>
2021-02-16 22:10:13 -0500

    Fixed some JS

--- SRPGCalc/script.js
+++ SRPGCalc/script.js
@@ -1,29 +1,42 @@
-console.log("Loaded script.js")
-
 
 
 function update_progress_bar(amount){
     let bar = document.getElementById('progress-bar');
     bar.setAttribute("aria-valuenow", amount);
     return false;
-}
-
-setInterval(function(){
-    let bar = document.getElementById('progress-bar');
-    let amount = bar.getAttribute("aria-valuenow");
-    update_progress_bar(amount+3);
-}, 3000);
+};
 
 
-document.getElementById('calculator-input').onsubmit = function() { 
-    console.log(document.getElementById('fights').value);
-    console.log(document.getElementById('stat2').value);
-    console.log(document.getElementById('stat3').value);
-    console.log(document.getElementById('stat4').value);
-    console.log(document.getElementById('stat5').value);
 
-    return false;
+
+function main(){
+
+    console.log("Loaded script.js");
+
+    document.getElementById('calculator-input').onsubmit = function() { 
+        console.log(document.getElementById('fights').value);
+        console.log(document.getElementById('stat2').value);
+        console.log(document.getElementById('stat3').value);
+        console.log(document.getElementById('stat4').value);
+        console.log(document.getElementById('stat5').value);
+    
+        return false;
+    
+    
+    
+    };
+
+    setInterval(function(){
+        let bar = document.getElementById('progress-bar');
+        let amount = bar.getAttribute("aria-valuenow");
+        update_progress_bar(amount+3);
+    }, 3000);
 
 
 
 };
+
+// A $( document ).ready() block.
+$( document ).ready(main);
+
+
